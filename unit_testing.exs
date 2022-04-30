d816c2190b7dd809a5a57d4d236653ba82b866b4
RafaelGutz444 <rgutierrez852@gmail.com>
2022-04-30 13:33:24 -0500

    Added comments throughout example code

--- unit_testing.exs
+++ unit_testing.exs
@@ -1,19 +1,20 @@
-ExUnit.start
+ExUnit.start			# set up test runner
 
 defmodule MyTest do
-	use ExUnit.Case
+
+	use ExUnit.Case		#use requires a module and sets up macros.
 
 	test 'simple test' do
-		assert 1 + 1 == 2
+		assert 1 + 1 == 2	#Elixir doesn't need assert_equal, etc. Will do comparison on its own.
 
 	end
 
-	test "refute is opposite of assert" do
+	test "refute is opposite of assert" do		#test macro accepts string as test name
 	refute 1 + 1 == 3
 
 	end
 	
-	test :assert_raise do
+	test :assert_raise do				# test macro also accepts an atom.
 		assert_raise ArithmeticError, fn ->
 			1 + "x"
 		end
@@ -21,8 +22,8 @@ defmodule MyTest do
 
 	test "assert_in_delta asserts that value1 and value2 differ by 
 less than delta." do
-	assert_in_delta 1,
-			5,
-			6
+	assert_in_delta 1,  # actual
+			5,  # expected
+			6   # delta
 	end
 end
